08f0c70867ec3a21d0cac14ae819524ffbab9336
Tom Balmas <tombalmas12@gmail.com>
2022-10-01 19:21:08 +0300

    adding add auth and info for users

---
+++ config/dbconfig.js
@@ -0,0 +1,4 @@
+module.exports = { 
+    secret: 'eztale',
+    database: 'mongodb+srv://root2:1234@maincluster.eegqfie.mongodb.net/EZTale'
+}
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const actions = require('../methods/actions');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.send('Hello World');
+})
+
+router.get('/home', (req,res) => {
+    res.send('Home');
+})
+
+//@desc Adding new user 
+//@route POST /adduser 
+router.post('/adduser', actions.addNew);
+//@desc Authenticate user
+//@route POST /auth
+router.post('/auth', actions.authenticate);
+//@desc Get user info
+//@route GET /getinfo
+router.get('/getinfo',actions.getInfo)
+
+module.exports = router;
