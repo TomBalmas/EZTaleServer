58c53dd85e3d7e43085a17b3fb208a823cff6d04
Tom Balmas <tombalmas12@gmail.com>
2023-01-03 14:11:58 +0200

    sever will not crush when exception thrown

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const passport = require('passport');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
-
-const { Schema } = mongoose;
-
-connectDB();
-
 const app = express();
 
 
-if(process.env.NODE_ENV === 'development') { 
-    app.use(morgan('dev'));
-}
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(routes);
-app.use(passport.initialize());
-require('./config/passport')(passport);
+startServer();
 
-const port = process.env.PORT || 3000;
-app.listen(port, console.log(`EZTale server listening in ${process.env.NODE_ENV} mode and port ${port}`));
+function startServer() {
+    try {
+        connectDB();
+        if (process.env.NODE_ENV === 'development') 
+            app.use(morgan('dev'));
+        app.use(cors());
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.use(routes);
+        app.use(passport.initialize());
+        require('./config/passport')(passport);
+        const port = process.env.PORT || 3000;
+        app.listen(port, console.log(`EZTale server listening in ${process.env.NODE_ENV} mode and port ${port}`));
+    }
+    catch (e) {
+        print(e);
+        startServer();
+    }
+}
+
